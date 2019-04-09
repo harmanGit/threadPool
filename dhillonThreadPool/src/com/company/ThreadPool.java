9dc95b16127fc141228d52e00135b60e53e06f57
Harman Dhillon <hsd77849@huskies.bloomu.edu>
2019-04-09 17:14:13 -0400

    works, can't remove threads

--- dhillonThreadPool/src/com/company/ThreadPool.java
+++ dhillonThreadPool/src/com/company/ThreadPool.java
@@ -4,39 +4,50 @@ import java.util.LinkedList;
 
 /**
  * Nested Class:
- *
+ * <p>
  * WorkQueue stores a linked list of runnable tasks. Use the LinkedList
  * class for this. Note that this call is not thread safe, which
  * is why you must synchronize access to it(see what happens if you
  * do not). You need one method to ass a task to the list and another
  * to get a task. A thread will block if it tries to get a task when
  * none is available.
- *
+ * <p>
  * WorkerThread implements the Runnable interface. Its run method simply
  * tries to get a task from the queue and run it.
- *
+ * <p>
  * The ThreadPool constructor creates an array of threads and instantiates
  * the work queue. It then fills the array with WorkThread objects and starts
  * them. the size of the array should be determined by a static constant
  * (initialized to 3 for testing purposes). The only other method needed for
  * your ThreadPool class is one that adds a given task to the queue.
- *
+ * <p>
  * Created by Harman Dhillon on 4/8/2019.
  */
 public class ThreadPool {
     private int threadPoolLimit;
+    private int numberOfConnections;
     private WorkQueue workQueue;
-    private WorkerThread workerThread;
+    private Thread[] workerThreads;
 
     public ThreadPool(int threadPoolLimit) {
         this.threadPoolLimit = threadPoolLimit;
         this.workQueue = new WorkQueue();
-        this.workerThread = new WorkerThread();
+        this.numberOfConnections = 0;
+        workerThreads = new Thread[threadPoolLimit];
+
+        for (int i = 0; i < this.threadPoolLimit; i++)
+            workerThreads[i] = new Thread(new WorkerThread());
+
+
     }
 
     public void add(Connection newConnection) {
         workQueue.addTask(newConnection);
-        workerThread.run();
+
+//        if(workerThreads[numberOfConnections] != null && !workerThreads[numberOfConnections].isAlive()){
+//            workQueue.runnableList.remove(numberOfConnections);
+            workerThreads[numberOfConnections].start();
+//        }
     }
 
     /**
@@ -51,11 +62,20 @@ public class ThreadPool {
 
         LinkedList<Runnable> runnableList;
 
-        public WorkQueue() {this.runnableList = new LinkedList<>();}
+        public WorkQueue() {
+            this.runnableList = new LinkedList<>();
+        }
 
-        private boolean addTask(Runnable task) {return runnableList.size() < threadPoolLimit && runnableList.add(task);}
+        private synchronized void addTask(Runnable task) {
+//            if(numberOfConnections <= threadPoolLimit){
+                runnableList.add(task);
+                numberOfConnections = runnableList.size() - 1;
+                System.err.println("asdfasd: "+ numberOfConnections);
+//            }
+//            return false;
+        }
 
-        private synchronized Runnable getTask(int indexOfTask) {
+        private Runnable getTask(int indexOfTask) {
             return runnableList.get(indexOfTask);
         }
     }
@@ -66,8 +86,7 @@ public class ThreadPool {
      */
     private class WorkerThread implements Runnable {
         public void run() {
-            for (int i = 0; i < threadPoolLimit; i++)
-                    workQueue.getTask(i).run();
+            workQueue.getTask(numberOfConnections).run();
         }
     }
 }
